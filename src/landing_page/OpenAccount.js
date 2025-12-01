1a23de7479e350f2a2900c596e9da398bf906954
Ashish1730a <nishantmishra281403@gmail.com>
2025-12-01 18:29:19 +0530

    Complete Pricing section, work on hero and Bokerage

--- src/landing_page/OpenAccount.js
+++ src/landing_page/OpenAccount.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function OpenAccount() {
     return ( 
-        <div className='container p-5 mt-5'>
+        <div className='container p-5 '>
              <div className='row text-center'>
                 <h5 className='mt-3 '>Open a Zerodha account</h5>
                 <p className='mt-3 '>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
