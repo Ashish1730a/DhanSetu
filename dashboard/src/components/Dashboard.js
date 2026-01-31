919b1d7d1075dee41d06ed592a46e2ddeefaf6a5
Ashish1730a <nishantmishra281403@gmail.com>
2026-01-31 11:54:31 +0530

    Added menu links feature

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -21,9 +21,9 @@ const Dashboard = () => {
       <div className="content">
         <Routes>
           <Route exact path="/" element={<Summary />} />
-          <Route path="/orders" element={<Orders />} />
+          <Route path="/order" element={<Orders />} />
           <Route path="/holdings" element={<Holdings />} />
-          <Route path="/positions" element={<Positions />} />
+          <Route path="/position" element={<Positions />} />
           <Route path="/funds" element={<Funds />} />
           <Route path="/apps" element={<Apps />} />
         </Routes>
